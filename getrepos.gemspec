973d510976b55b85d835c7b3b4204bf82dfbb784
Rogério Carvalho Schneider <stockrt@gmail.com>
2016-03-15 09:07:47 -0300

    Validate commands and JSON

--- getrepos.gemspec
+++ getrepos.gemspec
@@ -25,4 +25,5 @@ Gem::Specification.new do |spec|
   spec.require_paths = ['lib']
 
   spec.add_runtime_dependency('commander', '~> 4.4')
+  spec.add_runtime_dependency('colorize', '~> 0.7')
 end
--- lib/getrepos/install.rb
+++ lib/getrepos/install.rb
@@ -1,9 +1,13 @@
-require 'json'
-
 module GetRepos
   def install_repos(filename)
-    repos = JSON.parse(open(filename).read)
-    puts repos
+    repos = JSON.parse(open(filename).read, symbolize_names: true)
+    if repos[:repos].nil?
+      puts "Invalid JSON '#{filename}' (missing \"repos\" key)".light_red
+      exit 1
+    end
+    repos[:repos].each do |repo|
+      puts repo
+    end
   end
 
   module_function :install_repos
