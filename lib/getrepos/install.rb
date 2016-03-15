fd0314d45096cbfbb746cf6cc695da50fedad417
Rogério Carvalho Schneider <stockrt@gmail.com>
2016-03-15 13:24:31 -0300

    Download archives and prep for git

--- lib/getrepos/install.rb
+++ lib/getrepos/install.rb
@@ -1,7 +1,10 @@
 require 'getrepos/util'
 
 module GetRepos
+  module_function
+
   def install(filename)
+    FileUtils.mkdir_p('build')
     repos = JSON.parse(open(filename).read, symbolize_names: true)
 
     unless is_valid_json_root?(repos)
@@ -15,15 +18,63 @@ module GetRepos
         exit 1
       end
 
-      dest_dir = File.join('build', 'repos', repo[:name] + '-' + repo[:version])
-      git_bare_dest_dir = File.join('build', '.gitbare', repo[:name])
-      FileUtils.mkdir_p('build')
-      puts repo
+      puts 'Processing entry: '.light_cyan + repo.to_s
       if is_git_url?(repo[:url])
+        install_git(repo)
       else
+        archive = URI(repo[:url]).path
+        if archive.end_with?('.tar.gz')
+          install_archive(repo, 'tar.gz')
+        elsif archive.end_with?('.tar.bz2')
+          install_archive(repo, 'tar.bz2')
+        elsif archive.end_with?('.tar.xz')
+          install_archive(repo, 'tar.xz')
+        elsif archive.end_with?('.zip')
+          install_archive(repo, 'zip')
+        elsif archive.end_with?('.jar')
+          install_archive(repo, 'jar')
+        elsif archive.end_with?('.war')
+          install_archive(repo, 'war')
+        elsif archive.end_with?('.ear')
+          install_archive(repo, 'ear')
+        else
+          puts "Unsupported file extension in URL '#{repo[:url]}'".light_red
+          exit 1
+        end
       end
     end
   end
 
-  module_function :install
+  def install_git(repo)
+    dest_dir = File.join('build', 'repos', repo[:name] + '-' + repo[:version])
+    gitbare_dest_dir = File.join('build', '.gitbare', repo[:name])
+    puts dest_dir
+    puts gitbare_dest_dir
+  end
+
+  def install_archive(repo, ext)
+    dest_dir = File.join('build', 'repos', repo[:name] + '-' + repo[:version])
+    dest_file = File.join('build', 'repos', repo[:name] + '-' + repo[:version] + '.' + ext)
+    FileUtils.mkdir_p(dest_dir)
+
+    if File.exists?(dest_file)
+      puts "Already downloaded '#{repo[:url]}' to '#{dest_file}'".light_green
+    else
+      puts "Saving '#{repo[:url]}' to '#{dest_file}'".light_cyan
+      IO.copy_stream(open(repo[:url]), dest_file)
+    end
+
+    if ext == 'tar.gz'
+      %x(tar xzf #{dest_file} -C #{dest_dir})
+    elsif ext == 'tar.bz2'
+      %x(tar xjf #{dest_file} -C #{dest_dir})
+    elsif ext == 'tar.xz'
+      %x(tar xJf #{dest_file} -C #{dest_dir})
+    elsif ext == 'zip' || ext == 'jar' || ext == 'war' || ext == 'ear'
+      %x(unzip -o #{dest_file} -d #{dest_dir})
+    else
+      puts "Unsupported file extension '#{ext}'".light_red
+      exit 1
+    end
+  end
 end
--- lib/getrepos/util.rb
+++ lib/getrepos/util.rb
@@ -1,6 +1,6 @@
 def is_valid_json_root?(data)
   if data[:repos].nil?
-    puts 'Missing required "repos" key'.light_red
+    puts 'Missing required key "repos"'.light_red
     return false
   else
     return true
@@ -11,9 +11,17 @@ def is_valid_json_repo?(data)
   ret_flag = true
   [:name, :version, :url].each do |key|
     if data[key].nil?
-      puts "Missing required \"#{key}\" key".light_red
+      puts "Missing required key \"#{key}\"".light_red
       ret_flag = false
     end
   end
   return ret_flag
+end
+
+def is_git_url?(url)
+  if File.extname(URI(url).path) == '.git'
+    return true
+  else
+    return false
+  end
 end
